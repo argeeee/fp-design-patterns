62e53cb0fe1a01f08c4cf357021e60185220496e
argeeee <argetimjusufi01@gmail.com>
2023-10-29 18:40:29 +0100

    feat: ✨ added managed execution

--- src/StrategyPattern/StrategyPatternWithClasses.ts
+++ src/StrategyPattern/StrategyPatternWithClasses.ts
@@ -39,16 +39,18 @@ class ShoppingCart {
 }
 
 export default () => {
-	// Example usage
-	const creditCardStrategy = new CreditCardPayment("1234-5678-9876-5432", "John Doe");
-	const cartWithCreditCard = new ShoppingCart(creditCardStrategy);
-	cartWithCreditCard.checkout(100); // Pays with the credit card passed in the constructor
+	if (false) {
+		// Example usage
+		const creditCardStrategy = new CreditCardPayment("1111-1111-1111-1111", "John Doe");
+		const cartWithCreditCard = new ShoppingCart(creditCardStrategy);
+		cartWithCreditCard.checkout(100); // Pays with the credit card passed in the constructor
 
-	const payPalStrategy = new PayPalPayment("john.doe@example.com");
-	const cartWithPayPal = new ShoppingCart(payPalStrategy);
-	cartWithPayPal.checkout(50); // Pays with PayPal
+		const payPalStrategy = new PayPalPayment("john.doe@example.com");
+		const cartWithPayPal = new ShoppingCart(payPalStrategy);
+		cartWithPayPal.checkout(50); // Pays with PayPal
 
-	const cashStrategy = new CashPayment();
-	const cartWithCash = new ShoppingCart(cashStrategy);
-	cartWithCash.checkout(75); // Pays in cash
+		const cashStrategy = new CashPayment();
+		const cartWithCash = new ShoppingCart(cashStrategy);
+		cartWithCash.checkout(75); // Pays in cash
+	}
 }
--- src/StrategyPattern/StrategyPatternWithFunctions.ts
+++ src/StrategyPattern/StrategyPatternWithFunctions.ts
@@ -21,12 +21,14 @@ const checkout = (amount: number, paymentStrategy: PaymentStrategy) => {
 };
 
 export default () => {
-	// Example usage
-	const creditCardPayment = createCreditCardPayment("1234-5678-9876-5432", "John Doe");
-	checkout(100, creditCardPayment);
+	if (false) {
+		// Example usage
+		const creditCardPayment = createCreditCardPayment("1111-1111-1111-1111", "John Doe");
+		checkout(100, creditCardPayment);
 
-	const payPalPayment = createPayPalPayment("john.doe@example.com");
-	checkout(50, payPalPayment);
+		const payPalPayment = createPayPalPayment("john.doe@example.com");
+		checkout(50, payPalPayment);
 
-	checkout(75, createCashPayment);
+		checkout(75, createCashPayment);
+	}
 }
